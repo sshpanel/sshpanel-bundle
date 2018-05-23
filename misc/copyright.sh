8a9569d20c1818f981b7f60c87cd8264900354ac
Kodinger <itskodinger@gmail.com>
2018-05-23 07:18:51 +0700

    Fix Client path

--- misc/copyright.sh
+++ misc/copyright.sh
@@ -1,5 +1,9 @@
 . $HOME/sshpanel-bundle/support/string-helper.sh
 
+echo 
+echo
+echo
+
 info "Copyright (C) VPN Panel & SSH Panel."
 info "Powered by this awesome peoples! : "
 info "   - Nyr @ https://github.com/Nyr/openvpn-install"
